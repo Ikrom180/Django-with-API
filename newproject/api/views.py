9863b83548dbc116b01a9a8f93b6f76ed575074d
ikrom724 <ikrompersonal@gmail.com>
2026-09-05 18:00:55 +0500

    Rest api finished

--- newproject/api/views.py
+++ newproject/api/views.py
@@ -8,9 +8,12 @@ from .serializer import UserSerializer
 
 @api_view(['GET'])
 def get_user(request):
-    users = User.objects.all()
+    users = User.objects.get(id=1)
+    serializer = UserSerializer(users, many=False)
+    return Response(serializer.data)
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
+
 
 @api_view(['POST'])
 def create_user(request):
@@ -19,3 +22,29 @@ def create_user(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
